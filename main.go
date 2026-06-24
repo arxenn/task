a981dfa686073f9de4429e246cd2b3d44b48c97d
Alireza Hamedi <alireza12196@gmail.com>
2026-06-24 19:20:21 +0330

    fix version issue

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"runtime/debug"
+
 	"github.com/arxenn/task/cmd"
 )
-
-// TODO LIST:
-// - Complete windows shell integration
 
 var version = "dev"
 
 func main() {
-	cmd.Execute(version)
+	cmd.Execute(resolveVersion())
+}
+
+func resolveVersion() string {
+	if version != "dev" {
+		return version
+	}
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+	return "dev"
 }
